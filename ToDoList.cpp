3bcc41c650541f4049e8a42506c5c1d0ca4642b6
PratitaAnnisa <rahmaannisapra@gmail.com>
2026-04-21 01:16:36 +0700

    tambahan keterangan output

--- ToDoList.cpp
+++ ToDoList.cpp
@@ -3,5 +3,7 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
+    cout << "This is a simple Todo List application." << endl;
+    cout << "You can add tasks, view tasks, and mark tasks as completed." << endl;
     return 0;
 }
